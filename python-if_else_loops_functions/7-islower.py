dc4cdadb54f443746b6528fc7ef0504ddd91b21a
Jallah Sumbo <j.sumbo@alustudent.com>
2023-07-07 07:43:43 -0700

    task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def islower(c: str) -> bool:
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
+def uppercase(str):
+    for c in str:
+        isupper = ord(c) >= ord('a') and ord(c) <= ord('z')
+        if isupper:
+            # difference between 'a' and 'A'
+            diff = ord('A') - ord('a')
+            c = chr(ord(c) + diff)
+        print("{}".format(c), end='')
+    print()
