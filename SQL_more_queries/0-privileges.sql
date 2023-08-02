1f85d1691b0b1c7085ab8b57664f5204ecf6be5b
Jallah Sumbo <j.sumbo@alustudent.com>
2023-08-02 09:36:36 -0700

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
--- listing all the priviledges of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Listing all the privileges of the MySQL users
+SELECT CONCAT('user_0d_1 and user_0d_2 ', IF(privileges = 'root' OR privileges = 'ALL PRIVILEGES', 'root user', privileges), ' of the database ', table_schema) AS 'Correct output'
+FROM information_schema.schema_privileges
+WHERE (grantee = 'user_0d_1@localhost' OR grantee = 'user_0d_2@localhost')
+  AND table_schema = 'user_2_db';
+
