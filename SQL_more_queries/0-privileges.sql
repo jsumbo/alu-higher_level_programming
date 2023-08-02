2bdd29450368ae97aa43708e8dd4770ebbe2368f
Jallah Sumbo <j.sumbo@alustudent.com>
2023-08-02 09:40:10 -0700

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,16 @@
 -- Listing all the privileges of the MySQL users
-SELECT CONCAT('user_0d_1 and user_0d_2 ', IF(privileges = 'root' OR privileges = 'ALL PRIVILEGES', 'root user', privileges), ' of the database ', table_schema) AS 'Correct output'
-FROM information_schema.schema_privileges
-WHERE (grantee = 'user_0d_1@localhost' OR grantee = 'user_0d_2@localhost')
-  AND table_schema = 'user_2_db';
+-- Create user_0d_1 and grant all privileges to it
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
+-- Create user_0d_2 and grant specific privileges to it
+CREATE USER 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON database_name.table_name TO 'user_0d_2'@'localhost';
+-- Replace 'database_name' with the actual name of the database and 'table_name' with the actual name of the table.
+-- You can grant other privileges as needed based on your requirements.
+
+-- Listing privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Listing privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
